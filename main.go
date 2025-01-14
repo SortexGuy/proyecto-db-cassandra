9fe3dc5b966008bf4f43964bf3c5d464ba2cf8cb
Rubén A <sortexjuegos@gmail.com>
2025-01-14 14:23:52 -0400

    fix: Cors Added

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/SortexGuy/proyecto-db-cassandra/src/movies"
 	"github.com/SortexGuy/proyecto-db-cassandra/src/recommendations"
 	"github.com/SortexGuy/proyecto-db-cassandra/src/users"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -28,6 +29,7 @@ func main() {
 
 	// TODO: Execute code
 	r := gin.Default()
+	r.Use(cors.Default())
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
